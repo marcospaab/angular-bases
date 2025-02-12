4dec68cfd9163ffa2700daba21835d8e98825e1f
marcos.padin@outlook.es <you@example.com>
2025-02-12 11:37:34 +0100

    Fin de seccion 5

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { DbzModule } from '../dbz.module';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  standalone: false,
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+  constructor(private dbzService: DbzService) {
+
+  }
+
+  get characters(): Character[]{
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter( id: string): void{
+    this.dbzService.onDeleteCharacterById(id);
+  }
+
+  onNewCharacter( character: Character){
+    this.dbzService.addCharacter(character);
+  }
+}
